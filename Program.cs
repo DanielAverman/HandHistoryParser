c33a8cc12d46ef4cc786b8912960de725e748585
DanielAverman <danik17012000@mail.ru>
2025-11-24 21:04:37 +0300

    extract Config, cosmetic fixes in Parser, updated README

--- Program.cs
+++ Program.cs
@@ -1,22 +1,6 @@
 ﻿using HandHistoryParser.parser;
-using Microsoft.Extensions.DependencyInjection;
-using Serilog;
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Debug()
-    .WriteTo.File("logs/app.log",
-                  rollingInterval: RollingInterval.Day,
-                  outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}")
-    .CreateLogger();
-
-var serviceCollection = new ServiceCollection()
-    .AddLogging(builder =>
-        builder.AddSerilog(dispose: true))
-    .AddTransient<Parser>();
-
-var serviceProvider = serviceCollection.BuildServiceProvider();
-
-var handParser = serviceProvider.GetService<Parser>();
+Parser handParser = new();
 
 handParser?.ParseZipAndWriteOutput(Path.GetFullPath(@"resources\Cash._Holdem._NL25._2013._angrypaca._88k._107MB.zip"));
 
---
+++ utils/Config.cs
@@ -0,0 +1,35 @@
+﻿using HandHistoryParser.parser;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace HandHistoryParser.utils
+{
+    internal class Config
+    {
+        internal static ILogger<Parser> CreateDefaultLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File("logs/app.log",
+                              rollingInterval: RollingInterval.Day,
+                              outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}")
+                .CreateLogger();
+
+            return LoggerFactory.Create(builder =>
+            {
+                builder.AddSerilog(dispose: true);
+            }).CreateLogger<Parser>();
+        }
+
+        internal static string GetOutputFilePath(string zipFilePath)
+        {
+            string archiveName = Path.GetFileNameWithoutExtension(zipFilePath);
+            string dateString = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            string outputDirectory = Path.Combine("resources", "parsed");
+            string outputFileName = $"{dateString}_{archiveName}.txt";
+            string outputFilePath = Path.Combine(outputDirectory, outputFileName);
+            Directory.CreateDirectory(outputDirectory);
+            return outputFilePath;
+        }
+    }
+}
